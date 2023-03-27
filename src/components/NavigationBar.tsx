345401b57be9f04501b81a10a50950eaf92f2a54
Alex Withington-Smith <alex@awithsmith.com>
2023-03-27 22:36:06 +0100

    looking like design

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,13 +1,17 @@
 import logo from "../assets/logo.png";
+import instagramLogo from "../assets/instagram.svg";
+import twitterLogo from "../assets/twitter.svg";
+import down from "../assets/chevron-down-solid.svg";
+import globe from "../assets/globe-solid.svg";
 export function NavigationBar() {
   return (
-    <nav className="bg-red-50 w-full px-24 py-8 flex flex-row items-center justify-between border-black border-b">
+    <nav className="bg-red-50 w-full px-24 flex flex-row items-center justify-between border-black border-b">
       <img
         src={logo}
         className="object-scale-down h-36"
         alt="Safety First Wales logo - a red umbrella reading Safety First Wales over S1W"
       />
-      <ul className="flex flex-row gap-5">
+      <ul className="flex flex-row gap-10 items-center">
         <li>
           <a href="#" className="hover:text-gray-50">
             About Us
@@ -36,6 +40,31 @@ export function NavigationBar() {
         <li>
           <a href="#" className="hover:text-gray-50">
             Contact
+          </a>
+        </li>
+        <li>
+          <div className="flex flex-row gap-5 items-start">
+            <a href="#">
+              <img
+                src={instagramLogo}
+                alt="Instagram Logo"
+                className="h-8 hover:fill-gray-50"
+              />
+            </a>
+            <a href="#">
+              <img
+                src={twitterLogo}
+                alt="Twitter Logo"
+                className="h-8 hover:fill-gray-50"
+              />
+            </a>
+          </div>
+        </li>
+        <li>
+          <a href="#" className="flex flex-row items-center gap-1.5">
+            <img src={globe} alt="Image of a globe" className="h-5" />
+            English
+            <img src={down} alt="Down arrow on link" className="h-5" />
           </a>
         </li>
       </ul>
