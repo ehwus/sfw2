74254fe19407197ec7b63116be50fae52788ae12
Alex Withington-Smith <alex@awithsmith.com>
2023-03-27 22:19:01 +0100

    skeleton navbar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,3 +1,44 @@
+import logo from "../assets/logo.png";
 export function NavigationBar() {
-  return <div>Nav</div>;
+  return (
+    <nav className="bg-red-50 w-full px-24 py-8 flex flex-row items-center justify-between border-black border-b">
+      <img
+        src={logo}
+        className="object-scale-down h-36"
+        alt="Safety First Wales logo - a red umbrella reading Safety First Wales over S1W"
+      />
+      <ul className="flex flex-row gap-5">
+        <li>
+          <a href="#" className="hover:text-gray-50">
+            About Us
+          </a>
+        </li>
+        <li>
+          <a href="#" className="hover:text-gray-50">
+            News
+          </a>
+        </li>
+        <li>
+          <a href="#" className="hover:text-gray-50">
+            Briefing
+          </a>
+        </li>
+        <li>
+          <a href="#" className="hover:text-gray-50">
+            Petition
+          </a>
+        </li>
+        <li>
+          <a href="#" className="hover:text-gray-50">
+            Get Involved
+          </a>
+        </li>
+        <li>
+          <a href="#" className="hover:text-gray-50">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
