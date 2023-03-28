812d0f291485c37cfbc2fbfd4265812f929803e6
Alex Withington-Smith <alex@awithsmith.com>
2023-03-28 09:00:13 +0100

    slightly more responsive

--- src/components/BlackBox.tsx
+++ src/components/BlackBox.tsx
@@ -5,6 +5,8 @@ type BlackBoxProps = {
 };
 export function BlackBox({ handleClick, href, text }: BlackBoxProps) {
   return (
-    <div className="bg-black py-5 px-12 text-center text-white">{text}</div>
+    <div className="bg-black py-2.5 whitespace-nowrap px-6 text-center text-white">
+      {text}
+    </div>
   );
 }
--- src/components/DefaultHero.tsx
+++ src/components/DefaultHero.tsx
@@ -2,11 +2,11 @@ import petitionImage from "../assets/petition-image.png";
 import { BlackBox } from "./BlackBox";
 export function DefaultHero() {
   return (
-    <section className="w-full bg-red-50 px-24 flex flex-row">
+    <section className="w-full bg-red-50 px-24 lg:flex lg:flex-row">
       <img
         src={petitionImage}
         alt="A poster like promotional image for our petition - it reads 'decriminalise sex work in Wales, safety first!'"
-        className="h-96 p-12"
+        className="max-h-96 p-12"
       />
       <aside className="flex flex-col p-12">
         <h3 className="font-bold text-xl">#SAFETYFIRSTWALES</h3>
