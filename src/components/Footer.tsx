1c1d4a2d28e52c280d76f9a7ea7ecb1cd50179fe
Alex Withington-Smith <alex@awithsmith.com>
2023-06-27 17:31:36 +0100

    Footer amendments

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,4 @@
 import { useMemo } from "react";
-import paypalLogo from "../assets/paypal.png";
 import instagramLogo from "../assets/instagram.svg";
 import twitterLogo from "../assets/twitter.svg";
 
@@ -10,23 +9,25 @@ export function Footer() {
     <footer className="bg-red-100 w-full h-64 lg:h-fit px-8 py-6 lg:px-24 lg:py-12 flex flex-col lg:flex-row justify-between items-center mt-auto">
       <aside className="flex flex-col gap-1.5">
         <div className="flex gap-2.5 justify-center">
-          <a href="#">
+          <a
+            href="https://www.instagram.com/safety1stwales/"
+            target="_blank"
+            rel="noreferrer"
+          >
             <img src={instagramLogo} alt="Instagram logo" className="h-8" />
           </a>
-          <a href="#">
+          <a
+            href="https://twitter.com/safety1stwales"
+            target="_blank"
+            rel="noreferrer"
+          >
             <img src={twitterLogo} alt="Twitter logo" className="h-8" />
           </a>
         </div>
-        <p>contact@safetyfirstwales.org</p>
+        <p>safety1stwales@gmail.com</p>
       </aside>
       <aside className="order-last lg:order-none">
         <p>© Safety First Wales {currentYear}</p>
-      </aside>
-      <aside className="flex flex-col order-first lg:order-none">
-        <a href="#">
-          <img src={paypalLogo} alt="Paypal Logo" className="max-h-12" />
-          <p className="bg-black text-white px-1.5 text-center">Donate</p>
-        </a>
       </aside>
     </footer>
   );
