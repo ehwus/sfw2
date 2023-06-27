6ef270771fb8d3b115e1cabe89893fb196097890
Alex Withington-Smith <alex@awithsmith.com>
2023-06-27 20:01:10 +0100

    Welsh Proposals

---
+++ src/components/ProposalsWelsh.tsx
@@ -0,0 +1,106 @@
+import { Link } from "react-router-dom";
+
+export function ProposalsWelsh() {
+  return (
+    <section className="w-full flex flex-col lg:flex-row">
+      <h3 className="bg-black py-2.5 whitespace-nowrap px-6 text-center text-white w-64 h-11 mx-auto mt-5 lg:m-10">
+        CYNIGION AM NEWID
+      </h3>
+      <article className="lg:p-24 flex flex-col gap-5 m-5">
+        <h4 className="text-lg font-extrabold">
+          SAFETY FIRST WALES - CYNIGION AM NEWID
+        </h4>
+        <p className="text-justify">
+          Gallai rhai o'r camau hyn i wella diogelwch, iechyd a lles gweithwyr
+          rhyw gael eu gweithredu ar unwaith gan y Senedd ac mae eraill yn dod o
+          dan bwerau sydd heb eu datganoli. Yn yr achos hwnnw, gofynnwn i'r
+          Senedd gyhoeddi datganiad sy’n amlinellu ei safbwynt a'i bwriad i
+          godi'r materion hyn gyda San Steffan.
+        </p>
+        <p className="text-justify">
+          Cefnogi galwadau'n genedlaethol am ddad-droseddoli gwaith rhyw. Mae
+          tystiolaeth o Seland Newydd lle cyflwynwyd dad-droseddoli yn 2003 yn
+          dangos: dim cynnydd mewn puteindra; menywod mewn sefyllfa well i roi
+          gwybod am drais heb ofni cael eu harestio; clirio ymosodiadau'n
+          gyflymach; gweithwyr rhyw mewn sefyllfa well i adael puteindra gan fod
+          euogfarnau'n cael eu clirio o'u cofnodion; trin defnyddwyr cyffuriau
+          fel cleifion nid troseddwyr.
+        </p>
+        <p className="text-justify">
+          Gwrthwynebu troseddoli cleientiaid gweithwyr rhyw (a elwir yn 'fodel
+          Nordig') gan fod hyn yn tanseilio diogelwch gweithwyr rhyw.
+        </p>
+        <p className="text-justify">
+          Gweithredu amnest rhag cael eu harestio i amddiffyn gweithwyr rhyw
+          sy'n adrodd eu bod yn dyst i drais neu'n ei brofi. Enillodd ymgyrch
+          dan arweiniad gweithwyr rhyw y fath amnest yng Nghaliffornia. Mewn
+          dinasoedd ar draws yr Unol Daleithiau (Baltimore, Los Angeles,
+          Manhattan, Philadelphia, San Francisco) mae Twrnai Ardaloedd yn
+          gwrthod erlyn gweithwyr rhyw i gydnabod y niwed a achosir.
+        </p>
+        <p className="text-justify">
+          Diwygio canllawiau Cyngor Cenedlaethol Penaethiaid yr Heddlu i nodi y
+          dylai arestiadau a chyrchoedd ddigwydd dim ond pan all yr heddlu
+          ddangos seiliau rhesymol i amau bod gweithwyr rhyw yn destun grym a
+          gorfodaeth. Byddai hyn yn helpu i sicrhau nad yw gweithwyr rhyw yn
+          cael eu troseddoli dim ond am fod yn weithwyr rhyw neu am gymryd rhan
+          mewn arferion sy'n cynyddu eu diogelwch eu hunain.
+        </p>
+        <p className="text-justify">
+          Gweithredu 'wal dân' i wahanu ymchwiliadau'r heddlu ac archwiliadau
+          llafur gan swyddogion Gorfodi Mewnfudo, er mwyn sicrhau bod gweithwyr
+          rhyw mudol yn gallu adrodd am drais waeth beth fo'u statws mewnfudo.
+        </p>
+        <p className="text-justify">
+          Cyfarwyddo'r heddlu, yr awdurdodau erlyn a'r llysoedd i flaenoriaethu
+          diogelwch drwy ymchwilio'n egnïol i drais rhywiol, gorfodaeth,
+          stelcian a thrais arall, a'u herlyn.
+        </p>
+        <p className="text-justify">
+          Gwrthwynebu Cymal 16 (Atodlen 7 – Troseddau Blaenoriaeth) y Bil
+          Diogelwch Ar-lein a fydd i bob pwrpas yn atal gweithwyr rhyw rhag
+          hysbysebu ar-lein gan y dangoswyd bod hyn yn cynyddu'r risgiau i
+          weithwyr rhyw ac yn ei gwneud hi'n anoddach gweithio'n annibynnol.
+        </p>
+        <p className="text-justify">
+          Targedu adnoddau at weithwyr rhyw i'w galluogi i adael puteindra os
+          ydynt yn dewis hynny. Yn benodol, blaenoriaeth ar gyfer tai
+          cymdeithasol, rhyddhad dyledion, gwasanaethau iechyd, gwasanaethau
+          trais domestig gwell a chyfarwyddo'r rhai sy'n gwneud penderfyniadau
+          ynghylch budd-daliadau i beidio â gosod cosbau.
+        </p>
+        <p className="text-justify">
+          Ymestyn i famau fel "incwm gofal", y taliad o £1,600 y mis a
+          gyhoeddwyd yn ddiweddar ar gyfer pobl ifanc sy'n gadael gofal i
+          gydnabod y ffaith bod y rhan fwyaf o weithwyr rhyw yn gweithio i
+          gefnogi teuluoedd. Cymeradwyodd Hawaii gyfraith ym mis Ionawr i dalu
+          incwm sylfaenol o $2,000 y mis i weithwyr rhyw sydd am adael
+          puteindra.
+        </p>
+        <p className="text-justify">
+          Cymryd camau i atal mamau sy'n weithwyr rhyw rhag colli gwarchodaeth
+          o'u plant -- ni ddylid cymryd unrhyw blentyn oddi wrth ei fam/brif
+          ofalwr oherwydd bod y teulu'n dlawd, yn ddigartref neu'n methu talu am
+          fwyd, dillad neu ofal plant.
+        </p>
+        <p className="text-justify">
+          Cael gwared ar yr amod Heb Hawl i Gyllid Cyhoeddus (NRPF) sy'n
+          effeithio'n anghymesur ar fenywod ac yn eu gadael yn ddiymgeledd ac
+          mewn perygl o ecsbloetio a thrais.
+        </p>
+        <p className="text-justify">
+          Fel cam cyntaf uniongyrchol, gellid gweithredu argymhelliad gan y
+          Pwyllgor Seneddol Materion Cartref yn 2016 i ddad-droseddoli gweithwyr
+          rhyw ar y stryd a’r rhai sydd yn cydweithio i gael gwared ar gofnodion
+          troseddol.
+        </p>
+        <Link
+          to="/briefing"
+          className="border-2 border-black mx-auto py-4 px-8 rounded-full hover:opacity-75"
+        >
+          Gweler y briff llawn yma
+        </Link>
+      </article>
+    </section>
+  );
+}
