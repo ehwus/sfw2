b55c2c5dd0fa499a3fa78c199e2e8a0dfa64a6ac
Alex Withington-Smith <alex@awithsmith.com>
2023-06-27 17:22:36 +0100

    Hero Image in Welsh

--- src/components/DefaultHeroWelsh.tsx
+++ src/components/DefaultHeroWelsh.tsx
@@ -1,35 +1,35 @@
 import { Link } from "react-router-dom";
-import petitionImage from "../assets/petition-image.png";
+import petitionImageWelsh from "../assets/petition-image-welsh.jpg";
 
-export function DefaultHero() {
+export function DefaultHeroWelsh() {
   return (
     <section className="w-full bg-red-50 lg:px-24 lg:flex lg:flex-row mx-auto">
       <img
-        src={petitionImage}
+        src={petitionImageWelsh}
         alt="A poster like promotional image for our petition - it reads 'decriminalise sex work in Wales, safety first!'"
         className="max-h-96 lg:p-12 mx-auto"
       />
       <aside className="flex flex-col lg:p-12 p-8">
         <h3 className="font-bold text-xl">#SAFETYFIRSTWALES</h3>
         <p className="mt-5 text-justify">
-          Safety First Wales (SFW) – a coalition of sex workers, health
-          professionals, church representatives, anti-poverty, anti-violence,
-          and migrant and trans rights campaigners -- formed to decriminalise
-          sex work in Wales and prioritise sex workers’ safety, health, and
-          well-being.
+          Mae Safety First Wales yn gynghrair o weithwyr rhyw, gweithwyr iechyd
+          proffesiynol, cynrychiolwyr eglwysi, ymgyrchwyr gwrth-hiliol,
+          gwrthdlodi, gwrth-drais, mudwyr a hawliau traws -- a ffurfiwyd i
+          ddad-droseddoli gwaith rhyw yng Nghymru a blaenoriaethu diogelwch,
+          iechyd, a lles gweithwyr rhyw.
         </p>
         <div className="flex flex-wrap gap-5 mt-14 justify-center">
           <Link
             to="/about"
             className="bg-black py-2.5 whitespace-nowrap px-6 text-center text-white hover:opacity-75"
           >
-            Learn more
+            Dysgwch fwy
           </Link>
           <Link
             to="/petition"
             className="bg-black py-2.5 whitespace-nowrap px-6 text-center text-white hover:opacity-75"
           >
-            Sign the petition
+            Llofnodwch y ddeiseb
           </Link>
         </div>
       </aside>
--- src/pages/DefaultPage.tsx
+++ src/pages/DefaultPage.tsx
@@ -1,10 +1,14 @@
-import { DefaultHero } from "../components/DefaultHero";
+import { DefaultHeroEnglish } from "../components/DefaultHeroEnglish";
 import { RedQuote } from "../components/RedQuote";
+import { useBilingualism } from "../hooks/useBilingalism";
+import { DefaultHeroWelsh } from "../components/DefaultHeroWelsh";
 
 export function DefaultPage() {
+  const { language } = useBilingualism();
+
   return (
     <>
-      <DefaultHero />
+      {language == "English" ? <DefaultHeroEnglish /> : <DefaultHeroWelsh />}
       <RedQuote />
     </>
   );
